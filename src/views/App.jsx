b61ea5204095619ff9f300fc2a2d222c312c4f35
Leandro Lemes Vilela <leandrolvilela@gmail.com>
2023-08-17 20:46:22 -0300

    Configurando os css e estrutura dos projeto

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,9 +1,12 @@
+import Content from '../components/layout/Content';
+import Menu from '../components/layout/Menu';
 import './App.css'
 import React from "react";
 
 const App = props => (
     <div className="App">
-        Componente App
+        <Menu />
+        <Content/>
     </div>
 )
 
